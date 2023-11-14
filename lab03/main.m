44fc3e2c7b59fb929ff8946c7e7085f1288ecde3
TomaszBachorski <tomekbachorski@o2.pl>
2023-11-14 22:36:02 +0100

    small bug fix

--- lab03/main.m
+++ lab03/main.m
@@ -37,13 +37,13 @@ plot(lags, tx_rx);
 tx_rx(lags<0) = [];
 lags(lags<0) = [];
 [c_max,max_idx] = max(tx_rx);
-szukana = ((max_idx/2)/1000000)*speed_of_sound %0.3402 metra
+distance_of_single_period = ((max_idx/2)/1000000)*speed_of_sound %0.3402 metra
 
 clear all; close all; clc;
 %Sygnaly dlugie
 
 speed_of_sound = 340;
-Tp=1e-6;
+Tp=10^-6;
 rx = load('rx_long.dat');
 tx = load('tx_long.dat');
 figure; plot(rx);
@@ -56,14 +56,14 @@ figure;
 plot(lags,tx_rx);
 %200 - minimalna wysokosc peaku
 %10000 - minimalny dystans pomiedzy
-[v,x] = findpeaks(tx_rx, 'MinPeakHeight',200, 'MinPeakDistance', 10000);
+[v,x] = findpeaks(tx_rx, 'MinPeakHeight',200, 'MinPeakDistance',5000);
+findpeaks(tx_rx, 'MinPeakHeight',200, 'MinPeakDistance',5000);
 %Odleglosci w probkach
-x;
-distance = Tp*(x/2)*speed_of_sound;
+x=mod(x,10000) %10k probek jest w jednym okresie
+distance = Tp*(x/2)*speed_of_sound %przez dwa bo tam i spowrotem, oraz kazdy sygnal zostal nadany w roznym czasie
 %Odleglosc w metrach
 %Wynik wyglada sensownie bo w sumie im dalsza probka tym dluzej ten sygnal
 %musial isc wiec dystans powinien byc coraz wiekszy
-distance
 clear all; close all; clc;
 %Cwiczenie 2
 %kiedys kurwa zrobie bo pierdolca juz dostaje
@@ -85,6 +85,14 @@ plot(lags,sun_cor);
 findpeaks(sun_cor,'MinPeakDistance',85);
 %ponizsze liczby wektora x to dystans w miesiacach*2?
 [v, x] = findpeaks(sun_cor,'MinPeakDistance',85)
+
+for i=1:length(x)-1
+    breaks(i) = x(i+1)-x(i)
+end
+%average between peaks
+mean(breaks)
+
+
 
 
 
