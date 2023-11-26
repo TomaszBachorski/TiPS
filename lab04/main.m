b6e1a39fd32eb6b6ebf21502ca768abcfa5e742d
TomaszBachorski <tomekbachorski@o2.pl>
2023-11-26 14:12:33 +0100

    cwiczenie 1

--- lab04/main.m
+++ lab04/main.m
@@ -12,10 +12,10 @@ delta_f = 100/N;
 F = [0:N-1]*delta_f;
 
 %sygnal do pozniejszej rekonstrukcji
-signal = A * sawtooth(2*pi*f*T);
+signal = A * square(2*pi*f*T);
 
 % fft
-fourier_signal = 1/N*fft(signal); %1/N bo to wariant 1 z adasia
+fourier_signal = 1/N*fft(signal); %2/N bo to wariant 1 z adasia
 amplitudes = abs(fourier_signal); %sqrt(im^2 + re^2)
 phases = angle(fourier_signal); %przesuniecia fazowe
 
@@ -49,4 +49,73 @@ plot(T, reconstructed_signal, Color='Red');
 %Wniosek: zwiekszajac ilosc hormonicznych coraz bardziej zblizamy sygnal do
 %tego oryginalnego
 
+%Cwiczenie 2
+clear all; close all; clc;
+f_s=1/20;
+t_stop=2;
+t=0:f_s:t_stop-f_s;
+N = length(t);
+f=2;
+w=2*pi*f;
+A=1;
+
+phases = [0.1*pi 0.4*pi 1.5*pi];
+figure;
+%b
+for i=1:3
+    s=A*cos(w*t + phases(i));
+    ft=fft(s);
+    c=2*(ft/N);
+    m=abs(c);
+    pha = angle(c);
+    df = 1/t_stop;
+    fa = 0:N-1;
+    fa = fa*df;
+    subplot(3,2,2*i-1);
+    plot(fa,m);
+    subplot(3,2,2*i);
+    plot(fa,pha);
+end
+% Przy zmianie przesuniecia fazowego zmiania sie argument transformaty
+
+%c,d
+figure;
+f=[2,12,2.25];
+for i=1:3
+    w = 2*pi*f(i);
+    s=A*cos(w*t);
+    ft=fft(s);
+    c=2*(ft/N);
+    m=abs(c);
+    pha = angle(c);
+    df = 1/t_stop;
+    fa = 0:N-1;
+    fa = fa*df;
+    subplot(3,2,2*i-1);
+    plot(fa,m);
+    subplot(3,2,2*i);
+    plot(fa,pha);
+end
+%Patologia sie dzieje
+f_s=1/20;
+t_stop=1.75;
+t=0:f_s:t_stop-f_s;
+N = length(t);
+f=2;
+w=2*pi*f;
+A=1;
+s=A*cos(w*t);
+ft=fft(s);
+c=2*(ft/N);
+m=abs(c);
+pha = angle(c);
+df = 1/t_stop;
+fa = 0:N-1;
+fa = fa*df;
+figure;
+subplot(2,1,1);
+plot(fa,m);
+subplot(2,1,2);
+plot(fa,pha);
+%W sumie nic sie nie zmienilo
 
